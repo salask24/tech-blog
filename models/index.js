b7ada9104185a1279e425ad7226f0fa2d79a75bf
Kimberly Salas <salask0104@gmail.com>
2021-07-03 10:28:05 -0700

    added to models index.js

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+const { Model } = require('sequelize/types');
+
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+    //CASCADE - goes downward into all connections and deletes all from that user
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
