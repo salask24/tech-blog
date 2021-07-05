b9b141d4cb21fb894933fa0d33d7a5f8cd1ce311
Kimberly Salas <salask0104@gmail.com>
2021-07-05 06:48:16 -0700

    added routes to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
